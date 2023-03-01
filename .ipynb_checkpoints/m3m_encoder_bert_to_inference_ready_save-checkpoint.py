93def1f18be2cc9a8dbb8e312ca86d64bc375cf8
razzant <razant@yandex.ru>
2023-03-01 10:18:04 +0300

    remove cache

--- .ipynb_checkpoints/m3m_encoder_bert_to_inference_ready_save-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import torch
-from app.config import m3m  as m3m_config
-from app.kgqa.m3m import EncoderBERT
-
-
-if __name__ == '__main__':
-    encoder = torch.load("/home/salnikov/data/m3m/ckpts/encoder", map_location='cpu')
-    torch.save(encoder.state_dict(), '/home/salnikov/data/m3m/ckpts/encoder.pt')
