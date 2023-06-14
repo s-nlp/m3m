a6720f70ba1e03de0494da61d4e1dfa40b4b76c2
Mikhail Salnikov <Mikhail.Salnikov@skoltech.ru>
2023-06-14 15:09:43 +0300

    Remooved dublicates from m3m responces

--- app/pipelines/m3m.py
+++ app/pipelines/m3m.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 
+import numpy as np
 from fastapi import APIRouter
 from joblib import Parallel, delayed
 from pywikidata import Entity
@@ -22,10 +23,16 @@ m3m = M3MQA(**m3m_config)
 @router.post("/")
 def m3m_pipeline(question: QuestionRequest) -> M3MPipelineResponce:
     triples, predicts_filtered, scores_filtered, ue = m3m(question.text)
+    final_answers_idxs = []
+    predicts_filtered_set = []
+    for idx, pred in enumerate(predicts_filtered):
+        if pred not in predicts_filtered_set:
+            predicts_filtered_set.append(pred)
+            final_answers_idxs.append(idx)
 
     return M3MPipelineResponce(
-        answers=predicts_filtered[:60].tolist(),
-        scores=scores_filtered[:60].tolist(),
+        answers=np.array(predicts_filtered)[final_answers_idxs][:50].tolist(),
+        scores=np.array(scores_filtered)[final_answers_idxs][:50].tolist(),
         uncertenity=float(ue),
-        triples=triples[:60].tolist(),
+        triples=np.array(triples)[final_answers_idxs][:50].tolist(),
     )
